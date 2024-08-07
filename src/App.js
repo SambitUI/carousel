2031afd8b482115ccb33b4a2fc0d9a988218b6d7
SambitUI <sambit279@gmail.com>
2024-08-07 10:32:54 +0530

    imgcarousel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+/* Build a Highly Scalable Carousel Component in React JS
 
-function App() {
+ Requirements:
+  - We want to create a carousel component which takes array of images as input.
+  - The component should efficiently handle a large number of images while maintaining 
+  scalability, performance optimizations, and extensibility.
+  - Provide callback functions for events like image click, enabling users to define 
+  custom behavior.
+  - Focus on Accessibility.
+*/
+
+import { useEffect, useState } from "react";
+import "./App.css";
+import Carousel from "./Carousel";
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchImages = async (imgLimit) => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/photos?_limit=${imgLimit}`
+      );
+      const data = await response.json();
+      setImages(data);
+    } catch (error) {
+      console.error("Error fetching images:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages(8);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="carousel-container">
+      <Carousel
+        images={images}
+        isLoading={loading}
+        onImgClick={(image, index) => {}}
+        imgPerSlide={2}
+        imageLimit={4}
+        customPrevButton={(onClick) => (
+          <button
+            className="btn prev"
+            style={{ background: "red" }}
+            onClick={onClick}
+          >
+            Prev
+          </button>
+        )}
+        customNextButton={(onClick) => (
+          <button
+            className="btn next"
+            style={{ background: "blue" }}
+            onClick={onClick}
+          >
+            Next
+          </button>
+        )}
+      />
     </div>
   );
-}
+};
 
 export default App;
